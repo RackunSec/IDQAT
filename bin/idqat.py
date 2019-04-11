32a3d28fb7986b665421a3d902dac81f6e98c5c5
WeakNetLabs <weaknetlabs@gmail.com>
2019-04-11 14:23:24 -0400

    Updated for SysPII

--- bin/idqat.py
+++ bin/idqat.py
@@ -18,12 +18,16 @@ fileList = list() # all files found
 positivePiiFilesList = list()
 resultCount = 0
 
-# NO FALSE POSITIVES. OR ELSE!:
+# PII:
 reSSN = "[0-9]{3}[^0-9][0-9]{2}[^0-9][0-9]{4}"
 reDoB = "(0[0-9]|1[012])[^0-9][0-3][0-9][^0-9]([19|20][0-9]{2})"
 rePhone = "\+?1?[^0-9]?\(?[0-9]{3}\)?[^0-9]+[0-9]{3}[^0-9]+[0-9]{4}"
+# Financial Data:
+reCC = "[54][0-9]{3}[^0-9]([0-9]{4}[^0-9]){2}[0-9]{4}"
+# SysPII:
+reSerialNo = "([0-9]{5}-){4}[0-9]{5}"
 
-print "\nIDQAT - \n\n"
+print "\nIDQAT - \n"
 print "[*] Recursively searching current directory ... "
 
 # 1. Get the file list
@@ -50,9 +54,18 @@ while file < len(fileList):
 			print "[!] Phone number found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
 			positivePiiFilesList.append(fileList[file])
 			resultCount+=1
+		if re.search(reSerialNo,line):
+			print "[!] Possible serial number found in file: "+fileList[file]+" -> "+line # TODO remove
+			positivePiiFilesList.append(fileList[file])
+			resultCount+=1
+		if re.search(reCC,line):
+			print "[!] Possible credit card number found in file: "+fileList[file]+" -> "+line # TODO remove
+			positivePiiFilesList.append(fileList[file])
+			resultCount+=1
+
 	file += 1
 
 # 3. Log it in database
 
 # 4. Clean up and Exit
-print "[*] "+str(resultCount)+" results found.\n\n"
+print "\n[*] "+str(resultCount)+" results found.\n"
