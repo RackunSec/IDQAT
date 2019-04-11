db3cf848b858030b84e1186d86f8607fa37b92c3
WeakNetLabs <weaknetlabs@gmail.com>
2019-04-11 15:09:24 -0400

    Added file hash lib to idqat

--- bin/idqat.py
+++ bin/idqat.py
@@ -8,6 +8,7 @@
 import os # for opening files (OS independent)
 import re # for regular expressions
 import sys # for args
+import hashlib # Hashing file content
 
 if len(sys.argv)>1:
 	if re.search("-?-h(elp)?",sys.argv[1]):
@@ -26,6 +27,9 @@ rePhone = "\+?1?[^0-9]?\(?[0-9]{3}\)?[^0-9]+[0-9]{3}[^0-9]+[0-9]{4}"
 reCC = "[54][0-9]{3}[^0-9]([0-9]{4}[^0-9]){2}[0-9]{4}"
 # SysPII:
 reSerialNo = "([0-9]{5}-){4}[0-9]{5}"
+# my Functions:
+def fileHash(fh): # requires a file handle
+	return hashlib.md5(fh.read().hexdigest())
 
 print "\nIDQAT - \n"
 print "[*] Recursively searching current directory ... "
@@ -40,29 +44,38 @@ file = 0
 while file < len(fileList):
 	# print (fileList[file])
 	fh = open(fileList[file]) # open the file with a file handler
+	fileFoundPIIBool = 0 # if PII found, == 1
 	for line in fh:
 		line = line.rstrip()
 		if re.search(reSSN,line):
 			print "[!] SSN found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
 			positivePiiFilesList.append(fileList[file])
 			resultCount+=1
+			fileFoundPIIBool=1
 		if re.search(reDoB,line):
 			print "[!] DoB found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
 			positivePiiFilesList.append(fileList[file])
 			resultCount+=1
+			fileFoundPIIBool=1
 		if re.search(rePhone,line):
 			print "[!] Phone number found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
 			positivePiiFilesList.append(fileList[file])
 			resultCount+=1
+			fileFoundPIIBool=1
 		if re.search(reSerialNo,line):
 			print "[!] Possible serial number found in file: "+fileList[file]+" -> "+line # TODO remove
 			positivePiiFilesList.append(fileList[file])
 			resultCount+=1
+			fileFoundPIIBool=1
 		if re.search(reCC,line):
 			print "[!] Possible credit card number found in file: "+fileList[file]+" -> "+line # TODO remove
 			positivePiiFilesList.append(fileList[file])
 			resultCount+=1
-
+			fileFoundPIIBool=1
+	# rewind file and hash it if a positive was found:
+	if fileFoundPIIBool == 1:
+		# rewind read and pass fh to fileHash(fh) method.
+		pass
 	file += 1
 
 # 3. Log it in database
