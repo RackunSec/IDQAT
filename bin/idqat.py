9c8e9afa0844d747bfc96a0cc29d704f0b39eeb6
WeakNetLabs <weaknetlabs@gmail.com>
2019-04-11 13:32:41 -0400

    Updated idqat with new DoB regex

--- bin/idqat.py
+++ bin/idqat.py
@@ -4,25 +4,38 @@
 # Douglas Berdeaux
 # weaknetlabs@gmail.com
 #
+# OS Independent PII/SysPII Search and 
 import os
 import re
+
 fileList = list() # all files found
-piiFilesList = list()
+positivePiiFilesList = list()
+
+# NO FALSE POSITIVES. OR ELSE!:
 reSSN = "[0-9]{3}[^0-9][0-9]{2}[^0-9][0-9]{4}"
+reDoB = "(0[0-9]|1[012])[^0-9][0-3][0-9][^0-9]([19|20][0-9]{2})"
 
 print "\nIDQAT - \n\n"
 print "[*] Recursively searching current directory ... "
+
 # 1. Get the file list
 for root,directories, filenames in os.walk('.'):
 	for filename in filenames:
 		fileList.append(os.path.join(root,filename))
+
 # 2. Search each found file for PII
 file = 0
-
 while file < len(fileList):
 	# print (fileList[file])
 	fh = open(fileList[file]) # open the file with a file handler
 	for line in fh:
 		if re.search(reSSN,line):
-			print "[!] SSN Found in file: "+fileList[file]+" -> "+line
+			print "[!] SSN Found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			positivePiiFilesList.append(fileList[file])
+		if re.search(reDoB,line):
+			print "[!] DoB Found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			positivePiiFilesList.append(fileList[file])
 	file += 1
+
+# 3. Log it in database
+
