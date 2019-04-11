20aed6d48d3355bc217bd585c9a50dd18ecc4dbf
WeakNetLabs <weaknetlabs@gmail.com>
2019-04-11 13:53:14 -0400

    Added regex for phone numbers

--- bin/idqat.py
+++ bin/idqat.py
@@ -10,10 +10,12 @@ import re
 
 fileList = list() # all files found
 positivePiiFilesList = list()
+resultCount = 0
 
 # NO FALSE POSITIVES. OR ELSE!:
 reSSN = "[0-9]{3}[^0-9][0-9]{2}[^0-9][0-9]{4}"
 reDoB = "(0[0-9]|1[012])[^0-9][0-3][0-9][^0-9]([19|20][0-9]{2})"
+rePhone = "\+?1?[^0-9]?\(?[0-9]{3}\)?[^0-9]+[0-9]{3}[^0-9]+[0-9]{4}"
 
 print "\nIDQAT - \n\n"
 print "[*] Recursively searching current directory ... "
@@ -29,13 +31,22 @@ while file < len(fileList):
 	# print (fileList[file])
 	fh = open(fileList[file]) # open the file with a file handler
 	for line in fh:
+		line = line.rstrip()
 		if re.search(reSSN,line):
-			print "[!] SSN Found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			print "[!] SSN found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
 			positivePiiFilesList.append(fileList[file])
+			resultCount+=1
 		if re.search(reDoB,line):
-			print "[!] DoB Found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			print "[!] DoB found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
 			positivePiiFilesList.append(fileList[file])
+			resultCount+=1
+		if re.search(rePhone,line):
+			print "[!] Phone number found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			positivePiiFilesList.append(fileList[file])
+			resultCount+=1
 	file += 1
 
 # 3. Log it in database
 
+# 4. Clean up and Exit
+print "[*] "+str(resultCount)+" results found.\n\n"
