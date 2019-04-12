9ea0ef6c894cb82c8d5f6eb928d52a8bef56c235
root <root@demon>
2019-04-12 14:32:57 -0400

    OOP Pythn' u.p.g.r.a.y.e.d.d

--- bin/idqat.py
+++ bin/idqat.py
@@ -4,81 +4,109 @@
 # Douglas Berdeaux
 # weaknetlabs@gmail.com
 #
-# OS Independent PII/SysPII Search and
+# OS Independent PII/SysPII Query and Alert Tool
+#
+##
 import os # for opening files (OS independent)
 import re # for regular expressions
 import sys # for args
 import hashlib # Hashing file content
-
+##
+# Help Me!
+##
+argpath = ''
 if len(sys.argv)>1:
 	if re.search("-?-h(elp)?",sys.argv[1]):
 		print "\nIDQAT [Help]:\n\tUsage:\n\t\t./idqat.py <directory>\n"
 		sys.exit(1)
+	else: # must have been a path:
+		argpath = sys.argv[1]
+if argpath  == '':
+	argpath = '.'
 
-fileList = list() # all files found
-positivePiiFilesList = list()
-resultCount = 0
+# PII methods and values:
+class piiObject:
+	def __init__(self):
+		self.piiRegex = {} # associative array for readability, extensibility
+		self.piiRegex["reSSN"] = "[0-9]{3}[^0-9][0-9]{2}[^0-9][0-9]{4}"
+		self.piiRegex["reDoB"] = "(0[0-9]|1[012])[^0-9][0-3][0-9][^0-9]([19|20][0-9]{2})"
+		self.piiRegex["rePhone"] = "\+?1?[^0-9]?\(?[0-9]{3}\)?[^0-9]+[0-9]{3}[^0-9]+[0-9]{4}"
+		# Financial Data:
+		self.piiRegex["reCC"] = "[54][0-9]{3}[^0-9]([0-9]{4}[^0-9]){2}[0-9]{4}"
+		# SysPII:
+		self.piiRegex["reSerialNo"] = "([0-9]{5}-){4}[0-9]{5}"
+# File methods and values:
+class FileObject: # OOP!
+	def __init__(self):
+		# Globals values in Object:
+		self.fileList = list()
+		self.positivePiiFilesList = list()
+		self.fileResultCount = 0
+	# Hashing method:
+	def fileHash(self,fh): # requires a file handle
+		#return hashlib.md5(fh.read().hexdigest())
+		hash = hashlib.md5() # create a hash object
+		with fh: # iterate through file with a buffer, buf:
+			buf = fh.read()
+			hash.update(buf)
+		return hash.hexdigest() # return the final md5sum
+		# check with md5sum < filename
 
-# PII:
-reSSN = "[0-9]{3}[^0-9][0-9]{2}[^0-9][0-9]{4}"
-reDoB = "(0[0-9]|1[012])[^0-9][0-3][0-9][^0-9]([19|20][0-9]{2})"
-rePhone = "\+?1?[^0-9]?\(?[0-9]{3}\)?[^0-9]+[0-9]{3}[^0-9]+[0-9]{4}"
-# Financial Data:
-reCC = "[54][0-9]{3}[^0-9]([0-9]{4}[^0-9]){2}[0-9]{4}"
-# SysPII:
-reSerialNo = "([0-9]{5}-){4}[0-9]{5}"
-# my Functions:
-def fileHash(fh): # requires a file handle
-	return hashlib.md5(fh.read().hexdigest())
+pii = piiObject() # instantiate object from class
+files = FileObject() # instantiate oibject from class
 
+##
+# Workflow of the App:
+##
 print "\nIDQAT - \n"
-print "[*] Recursively searching current directory ... "
+print "[*] Recursively searching directory "+argpath+" ... "
 
 # 1. Get the file list
-for root,directories, filenames in os.walk('.'):
+for root,directories, filenames in os.walk(argpath):
 	for filename in filenames:
-		fileList.append(os.path.join(root,filename))
+		files.fileList.append(os.path.join(root,filename))
 
 # 2. Search each found file for PII
 file = 0
-while file < len(fileList):
+while file < len(files.fileList):
 	# print (fileList[file])
-	fh = open(fileList[file]) # open the file with a file handler
+	fh = open(files.fileList[file]) # open the file with a file handler
 	fileFoundPIIBool = 0 # if PII found, == 1
 	for line in fh:
 		line = line.rstrip()
-		if re.search(reSSN,line):
-			print "[!] SSN found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
-			positivePiiFilesList.append(fileList[file])
-			resultCount+=1
+		if re.search(pii.piiRegex["reSSN"],line):
+			print "[!] SSN found in file: "+files.fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			files.positivePiiFilesList.append(files.fileList[file])
+			files.fileResultCount+=1
 			fileFoundPIIBool=1
-		if re.search(reDoB,line):
-			print "[!] DoB found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
-			positivePiiFilesList.append(fileList[file])
-			resultCount+=1
+		if re.search(pii.piiRegex["reDoB"],line):
+			print "[!] DoB found in file: "+files.fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			files.positivePiiFilesList.append(files.fileList[file])
+			files.fileResultCount+=1
 			fileFoundPIIBool=1
-		if re.search(rePhone,line):
-			print "[!] Phone number found in file: "+fileList[file]+" -> "+line # TODO remove printing of PII to stdout
-			positivePiiFilesList.append(fileList[file])
-			resultCount+=1
+		if re.search(pii.piiRegex["rePhone"],line):
+			print "[!] Phone number found in file: "+files.fileList[file]+" -> "+line # TODO remove printing of PII to stdout
+			files.positivePiiFilesList.append(files.fileList[file])
+			files.fileResultCount+=1
 			fileFoundPIIBool=1
-		if re.search(reSerialNo,line):
-			print "[!] Possible serial number found in file: "+fileList[file]+" -> "+line # TODO remove
-			positivePiiFilesList.append(fileList[file])
-			resultCount+=1
+		if re.search(pii.piiRegex["reSerialNo"],line):
+			print "[!] Possible serial number found in file: "+files.fileList[file]+" -> "+line # TODO remove
+			files.positivePiiFilesList.append(files.fileList[file])
+			files.fileResultCount+=1
 			fileFoundPIIBool=1
-		if re.search(reCC,line):
-			print "[!] Possible credit card number found in file: "+fileList[file]+" -> "+line # TODO remove
-			positivePiiFilesList.append(fileList[file])
-			resultCount+=1
+		if re.search(pii.piiRegex["reCC"],line):
+			print "[!] Possible credit card number found in file: "+files.fileList[file]+" -> "+line # TODO remove
+			files.positivePiiFilesList.append(files.fileList[file])
+			files.fileResultCount+=1
 			fileFoundPIIBool=1
 	# rewind file and hash it if a positive was found:
 	if fileFoundPIIBool == 1:
 		# rewind read and pass fh to fileHash(fh) method.
-		pass
+		fh.seek(0,0) # rewind
+		print files.fileHash(fh)
 	file += 1
 
 # 3. Log it in database
 
 # 4. Clean up and Exit
-print "\n[*] "+str(resultCount)+" results found.\n"
+print "\n[*] "+str(files.fileResultCount)+" results found.\n"
