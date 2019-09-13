60bc62f44bec1883f0d65ebf9b28eeac409afb20
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-13 10:26:29 -0400

    Create login.php

---
+++ IDQAT CO/html/admin/login.php
@@ -0,0 +1,12 @@
+<?php
+  # Handle the login process, log it all, block offenders, lock accounts, etc
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>IDQAT CO Login</title>
+</head>
+<body>
+  <!-- login form, submits to me, handled by me at the top. -->
+</body>
+</html>
